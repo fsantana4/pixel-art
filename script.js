11423405cb69222a5cc73d02d09d2f3789e087dc
fsantana4 <fernando.santana@cronapp.io>
2024-10-24 21:47:50 -0300

    Pixel Art em JS

---
+++ script.js
@@ -0,0 +1,141 @@
+const canvas = document.querySelector(".canvas");
+const inputSize = document.querySelector(".input-size");
+const inputColor = document.querySelector(".input-color");
+const usedColors = document.querySelector(".used-colors");
+const buttonSave = document.querySelector(".button-save");
+const colResize = document.querySelector(".resize");
+const main = document.querySelector("main");
+
+const MIN_CANVAS_SIZE = 4;
+
+let isPainting = false;
+let isResizing = false;
+let undoStack = [];
+
+const createElement = (tag, className = "") => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+};
+
+const saveCanvasState = () => {
+    undoStack.push(canvas.innerHTML); 
+};
+
+const undoLastAction = () => {
+    if (undoStack.length > 0) {
+        const lastState = undoStack.pop(); 
+        canvas.innerHTML = lastState; 
+        addEventListenersToPixels(); 
+    }
+};
+
+const setPixelColor = (pixel) => {
+    pixel.style.backgroundColor = inputColor.value;
+    saveCanvasState(); 
+};
+
+const createPixel = () => {
+    const pixel = createElement("div", "pixel");
+
+    pixel.addEventListener("mousedown", () => setPixelColor(pixel));
+    pixel.addEventListener("mouseover", () => {
+        if (isPainting) setPixelColor(pixel);
+    });
+
+    return pixel;
+};
+
+const loadCanvas = () => {
+    const length = inputSize.value;
+    canvas.innerHTML = "";
+
+    for (let i = 0; i < length; i += 1) {
+        const row = createElement("div", "row");
+
+        for (let j = 0; j < length; j += 1) {
+            row.append(createPixel());
+        }
+
+        canvas.append(row);
+    }
+    saveCanvasState(); 
+};
+
+const addEventListenersToPixels = () => {
+    const pixels = canvas.querySelectorAll(".pixel");
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mousedown", () => setPixelColor(pixel));
+        pixel.addEventListener("mouseover", () => {
+            if (isPainting) setPixelColor(pixel);
+        });
+    });
+};
+
+const updateCanvasSize = () => {
+    if (inputSize.value >= MIN_CANVAS_SIZE) {
+        loadCanvas();
+    }
+};
+
+const changeColor = () => {
+    const button = createElement("button", "button-color");
+    const currentColor = inputColor.value;
+
+    button.style.backgroundColor = currentColor;
+    button.setAttribute("data-color", currentColor);
+    button.addEventListener("click", () => (inputColor.value = currentColor));
+
+    const savedColors = Array.from(usedColors.children);
+
+    const check = (btn) => btn.getAttribute("data-color") != currentColor;
+
+    if (savedColors.every(check)) {
+        usedColors.append(button);
+    }
+};
+
+const resizeCanvas = (cursorPositionX) => {
+    if (!isResizing) return;
+
+    const canvasOffset = canvas.getBoundingClientRect().left;
+    const width = `${cursorPositionX - canvasOffset - 20}px`;
+
+    canvas.style.maxWidth = width;
+    colResize.style.height = width;
+};
+
+const saveCanvas = () => {
+    html2canvas(canvas, {
+        onrendered: (image) => {
+            const img = image.toDataURL("image/png");
+            const link = createElement("a");
+
+            link.href = img;
+            link.download = "pixelart.png";
+
+            link.click();
+        },
+    });
+};
+
+canvas.addEventListener("mousedown", () => (isPainting = true));
+canvas.addEventListener("mouseup", () => (isPainting = false));
+
+inputSize.addEventListener("change", updateCanvasSize);
+inputColor.addEventListener("change", changeColor);
+
+colResize.addEventListener("mousedown", () => (isResizing = true));
+
+main.addEventListener("mouseup", () => (isResizing = false));
+main.addEventListener("mousemove", ({ clientX }) => resizeCanvas(clientX));
+
+buttonSave.addEventListener("click", saveCanvas);
+
+document.addEventListener("keydown", (event) => {
+    if (event.ctrlKey && event.key === 'z') {
+        undoLastAction();
+    }
+});
+
+loadCanvas();
